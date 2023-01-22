4883e2973f8fb29616758fba96dc983198034807
abdelrahmanhossam05 <abouadmaa@gmail.com>
2023-01-21 23:53:38 -0400

    Loops

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-let celcius = 12;
-let fahrenheit = celcius * 1.8 + 32;
-
-console.log("The temperature " + celcius + " in celcius is " + fahrenheit + " in fahrenheit")
+for (let i=1; i<=20; i++) {
+    if (i%3 === 0 && i%5 === 0) {
+        console.log("Frontend Simplified");
+    }
+     else if (i%3 === 0) {
+        console.log("Frontend");
+    }
+    else if (i%5 === 0) {
+        console.log("Simplified");
+    }
+    else {
+        console.log(i);
+    }
+}
