abf4c2ebe5454c9bca806cb59afcd03393e31897
abdelrahmanhossam05 <abouadmaa@gmail.com>
2023-01-27 23:02:34 -0400

    new functions

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 // First way of accessing element
-document.querySelector('#title').innerHTML = "Hossam Abouadma"
+document.querySelector('#title').innerHTML += " Hossam Abouadma"
 
 // Second way of accessing element
 // document.getElementById('title')
+
+//change css
+// document.querySelector('#title').style.fontSize = '16px'
+
+// function changeTitleToRed() {
+//     document.querySelector('#title').style.color = 'red'
+//     console.log('clicked')
+// }
+
+function toggleDarkMode() {
+    document.querySelector('body').classList.toggle('dark-theme')
+}
