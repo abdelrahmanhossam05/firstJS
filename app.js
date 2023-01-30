49372dd4c9d9fd02c1200db7411dc7a23ac9b0cb
abdelrahmanhossam05 <abouadmaa@gmail.com>
2023-01-29 21:35:11 -0400

    js sum

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-// fetch('https://jsonplaceholder.typicode.com/users/1')
-const emailRef = document.querySelector('.email')
-// 1. Then
-// fetch('https://jsonplaceholder.typicode.com/users/1').then(response => {
-//     response.json().then(data => {
-//         emailRef.innerHTML = data.email;
-//     })
-// })
+// // fetch('https://jsonplaceholder.typicode.com/users/1')
+// const emailRef = document.querySelector('.email')
+// // 1. Then
+// // fetch('https://jsonplaceholder.typicode.com/users/1').then(response => {
+// //     response.json().then(data => {
+// //         emailRef.innerHTML = data.email;
+// //     })
+// // })
 
-// 2. Async/Await
-async function main() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
-    const data = await response.json()
-    emailRef.innerHTML = data.email
+// // 2. Async/Await
+// async function main() {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+//     const data = await response.json()
+//     emailRef.innerHTML = data.email
+// }
+
+// main();
+
+function sum(num1, num2){
+    return num1+num2;
 }
-
-main();
